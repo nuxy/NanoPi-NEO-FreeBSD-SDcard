f5ca7f9697a44932218b13ba36149bcac827ca0e
Marc S. Brooks <marc@mbrooks.info>
2020-03-28 22:52:58 -0700

    Added FreeBSD partition customizations

--- config.sh
+++ config.sh
@@ -3,7 +3,8 @@ board_setup NanoPi-NEO
 option ImageSize 3000mb
 option User nanopi-neo
 
-# Runs after FreeBSD partition is built and populated.
 customize_freebsd_partition ( ) {
-	chown root:wheel /etc/rc.conf
+    chown -R root:wheel loader.conf
+    chown -R root:wheel etc
+    chown -R root:wheel home
 }
