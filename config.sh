de099540c5fd850e8a846bf51ba8fb539743589a
Marc S. Brooks <marc@mbrooks.info>
2020-03-28 23:53:51 -0700

    Added portsnap install config option

--- config.sh
+++ config.sh
@@ -1,8 +1,9 @@
 board_setup NanoPi-NEO
 
 option ImageSize 3000mb
-option User nanopi-neo
 option SwapFile 2000mb [deferred] [file=/swap]
+option User nanopi-neo
+option UsrPorts
 
 customize_freebsd_partition() {
     chown -R root:wheel loader.conf
