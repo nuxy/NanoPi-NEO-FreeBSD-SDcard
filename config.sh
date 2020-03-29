24c18ea836e80a152be6b85469fe40e78916e10d
Marc S. Brooks <marc@mbrooks.info>
2020-03-29 04:19:57 -0700

    Fixed invalid SwapFile arguments
    Updated / group permissions

--- config.sh
+++ config.sh
@@ -1,12 +1,13 @@
 board_setup NanoPi-NEO
 
 option ImageSize 3000mb
-option SwapFile 2000mb [deferred] [file=/swap]
+option SwapFile 2000mb deferred file=/swap
 option User nanopi-neo
 option UsrPorts
 
 customize_freebsd_partition() {
-    chown -R root:wheel loader.conf
+    chown    root:wheel /
+    chown -R root:wheel boot
     chown -R root:wheel etc
     chown -R root:wheel home
 }
