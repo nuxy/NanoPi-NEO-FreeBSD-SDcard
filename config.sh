93b92a3c8b7eefbd742c55029a8e1cdd0ff6da85
Marc S. Brooks <marc@mbrooks.info>
2020-03-30 21:02:49 -0700

    Added PackageInit URL and related Package

--- config.sh
+++ config.sh
@@ -3,6 +3,8 @@ board_setup NanoPi-NEO
 option ImageSize 3000mb
 option SwapFile 2000mb deferred file=/swap
 option User nanopi-neo
+option PackageInit http://pkg.freebsd.org/FreeBSD:12:armv7/quarterly/
+option Package node npm sqlite3
 option UsrPorts
 
 IMGNAME=FreeBSD.img
