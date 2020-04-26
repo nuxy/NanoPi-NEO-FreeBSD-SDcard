038c9517f7966b2c19acfb8340050e6e585475e4
Marc S. Brooks <marc@mbrooks.info>
2020-04-26 10:39:09 -0700

    Removed FTD make/copy strategies (use port)

--- NanoPi-NEO/setup.sh
+++ NanoPi-NEO/setup.sh
@@ -8,10 +8,6 @@ FREEBSD_SRC=/usr/src
 FREEBSD_SYS=${FREEBSD_SRC}/sys
 IMAGE_SIZE=$((1000 * 1000 * 1000))
 
-npi_dts_dir=/usr/src/sys/gnu/dts/arm
-npi_dts_file_base=sun8i-h3-nanopi-neo
-npi_dts_full_path=${npi_dts_dir}/${npi_dts_file_base}.dts
-
 allwinner_partition_image() {
 	echo "Installing U-Boot from: ${UBOOT_PATH}"
 
@@ -26,50 +22,12 @@ allwinner_check_uboot() {
 	uboot_port_test ${SUNXI_UBOOT} ${SUNXI_UBOOT_BIN}
 }
 
-make_workaround_fdt() {
-	mkdir -p ${WORKDIR}/npi
-
-	cmd=`echo MACHINE=arm ${FREEBSD_SYS}/tools/fdt/make_dtb.sh ${FREEBSD_SYS} ${npi_dts_full_path} ${WORKDIR}/npi`
-	sh -c "$cmd"
-	echo $cmd
-
-	if [ $? != 0 ]; then
-		echo "make_workaround_fdt: Command failed."
-		exit 1
-	fi
-}
-
-copy_workaround_fdt() {
-	destdir=$1
-
-	echo "Copy device tree (dtb) to: ${destdir}/${npi_dts_file_base}.dtb"
-
-	if [ x = "x$destdir" ]; then
-		echo "copy_workaround_fdt: Needs a destination directory argument."
-		exit 1
-	fi
-
-	case "x$destdir" in
-		xboot)
-			destdir=${BOARD_BOOT_MOUNTPOINT}
-			;;
-		xbsd)
-			destdir=${BOARD_FREEBSD_MOUNTPOINT}/boot/dtb
-			;;
-	esac
-
-	if [ ! -d $destdir ]; then
-		echo "copy_workaround_fdt: $destdir is not a directory."
-		exit 1
-	fi
-
-	cp ${WORKDIR}/npi/${npi_dts_file_base}.dtb ${destdir}/${npi_dts_file_base}.dtb
-}
-
-make_boot_install_boot_scr_file() {
-	echo "echo \"Loading U-boot loader: ubldr.bin\"" > ${BOARD_BOOT_MOUNTPOINT}/boot.cmd
-	echo "load \${devtype} \${devnum}:${distro_bootpart}" "${UBLDR_LOADADDR}" ubldr.bin >> ${BOARD_BOOT_MOUNTPOINT}/boot.cmd
-	echo "go ${UBLDR_LOADADDR}" >> ${BOARD_BOOT_MOUNTPOINT}/boot.cmd
+make_install_boot_scr_file() {
+	cat << EOF > ${BOARD_BOOT_MOUNTPOINT}/boot.cmd
+echo "Loading U-boot loader: ubldr.bin"
+load \${devtype} \${devnum}:\${distro_bootpart} ${UBLDR_LOADADDR} ubldr.bin
+go ${UBLDR_LOADADDR}
+EOF
 
 	mkimage -A arm -T script -C none -n "Boot Commands" -d ${BOARD_BOOT_MOUNTPOINT}/boot.cmd ${BOARD_BOOT_MOUNTPOINT}/boot.scr
 }
@@ -78,10 +36,7 @@ strategy_add $PHASE_PARTITION_LWW allwinner_partition_image
 strategy_add $PHASE_CHECK allwinner_check_uboot
 strategy_add $PHASE_BUILD_OTHER freebsd_ubldr_build UBLDR_LOADADDR=${UBLDR_LOADADDR}
 strategy_add $PHASE_BOOT_INSTALL freebsd_ubldr_copy_ubldr .
-strategy_add $PHASE_BOOT_INSTALL make_workaround_fdt
-strategy_add $PHASE_BOOT_INSTALL copy_workaround_fdt boot
-strategy_add $PHASE_FREEBSD_BOARD_INSTALL copy_workaround_fdt bsd
-strategy_add $PHASE_FREEBSD_BOARD_INSTALL make_boot_install_boot_scr_file
+strategy_add $PHASE_FREEBSD_BOARD_INSTALL make_install_boot_scr_file
 
 # Put the kernel on the FreeBSD UFS partition.
 strategy_add $PHASE_FREEBSD_BOARD_INSTALL board_default_installkernel .
