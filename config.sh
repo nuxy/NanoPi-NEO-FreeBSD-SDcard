1881c65e5dbe9ce856dcd4b2ab0b0c069bce2fbc
Marc S. Brooks <marc@mbrooks.info>
2020-04-26 10:48:44 -0700

    Install package manager/Qemu during config process

--- config.sh
+++ config.sh
@@ -7,6 +7,7 @@ IMGNAME=FreeBSD.img
 SRCCONF=${PWD}/NanoPi-NEO/overlay/etc/src.conf
 
 customize_freebsd_partition() {
+	pkg install -y u-boot-qemu-arm
 
 	# Enable kernel binary image activator.
 	binmiscctl add armelf --interpreter "/usr/local/bin/qemu-arm-static" --magic "\x7f\x45\x4c\x46\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00" --mask "\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff" --size 20 --set-enabled
@@ -17,10 +18,11 @@ customize_freebsd_partition() {
 	chroot . /usr/local/bin/qemu-arm-static /bin/sh <<EOF
 		service ldconfig start
 
-		pkg install -y node npm python2 sqlite3
+		export ASSUME_ALWAYS_YES=yes
 
-		cd /server
-		LZZ_COMPAT=1 npm install --no-audit --no-optional --unsafe
+		pkg bootstrap && pkg install git-lite node npm python2 sqlite3
+
+		npm install --cwd /server --prefix /server --no-audit --no-optional --unsafe
 
 		pw groupadd -g 1973 -n nanopi-neo
 		pw useradd  -u 1973 -n nanopi-neo -g nanopi-neo -d /nonexistent -s /sbin/nologin
