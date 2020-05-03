51d1b683c48df2c5097739a1e1075d2bc4eaa0e1
Marc S. Brooks <marc@mbrooks.info>
2020-05-03 07:00:53 -0700

    Added EFI boot loader workaround
    Removed EFI mounting strategy

--- NanoPi-NEO2/setup.sh
+++ NanoPi-NEO2/setup.sh
@@ -13,7 +13,7 @@ allwinner_partition_image() {
 	dd if=${UBOOT_PATH}/${SUNXI_UBOOT_BIN} conv=notrunc,sync of=/dev/${DISK_MD} bs=1024 seek=8
 
 	disk_partition_mbr
-	disk_fat_create 64m 16 1m
+	disk_fat_create 32m 16 1m
 	disk_ufs_create
 }
 
@@ -22,30 +22,22 @@ allwinner_check_uboot() {
 }
 
 allwinner_install_uboot() {
-    echo bootaa64 > startup.nsh
+	echo bootaa64 > startup.nsh
 
-    mkdir -p EFI/BOOT
-    cp ${UBOOT_PATH}/${SUNXI_UBOOT_BIN} .
-    cp ${UBOOT_PATH}/README .
+	mkdir -p EFI/BOOT
+}
 
-	cat << EOF > config.txt
-arm_control=0x200
-dtparam=audio=on,i2c_arm=on,spi=on
-dtoverlay=mmc
-dtoverlay=pi3-disable-bt
-device_tree_address=0x4000
-kernel=u-boot.bin
-EOF
+copy_boot_loader_efi_file() {
+	echo "Installing loader.efi in ${TARGET}"
+
+	cp ${BOARD_FREEBSD_MOUNTPOINT}/boot/loader.efi EFI/BOOT/bootaa64.efi
 }
 
 strategy_add $PHASE_PARTITION_LWW allwinner_partition_image
 strategy_add $PHASE_CHECK allwinner_check_uboot
 strategy_add $PHASE_BOOT_INSTALL allwinner_install_uboot
 strategy_add $PHASE_BUILD_OTHER freebsd_loader_efi_build
-strategy_add $PHASE_BOOT_INSTALL freebsd_loader_efi_copy EFI/BOOT/bootaa64.efi
+strategy_add $PHASE_BOOT_INSTALL copy_boot_loader_efi_file
 
 # Put the kernel on the FreeBSD UFS partition.
 strategy_add $PHASE_FREEBSD_BOARD_INSTALL board_default_installkernel .
-
-# overlay/etc/fstab mounts the FAT partition at /boot/efi
-strategy_add $PHASE_FREEBSD_BOARD_INSTALL mkdir -p boot/efi
